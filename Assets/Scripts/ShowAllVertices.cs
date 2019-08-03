4cafcc45a2bc691c3a0c4b492fc5bd4a4f19b51e
Ece Sefercioğlu <seferciogluecce@users.noreply.github.com>
2019-08-03 15:18:29 +0300

    Project init

---
+++ Assets/Scripts/ShowAllVertices.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowAllVertices : MonoBehaviour
+{
+
+    List<Vector3> VerticeList = new List<Vector3>();
+    List<Vector3> VerticeListToShow = new List<Vector3>();
+    public float sphereSize = 0.5f;
+
+    List<Color> Colors = new List<Color>() { Color.red, Color.blue, Color.yellow, Color.green, Color.cyan, Color.magenta,Color.gray,Color.black,Color.white,Color.red,Color.blue };
+    void OnDrawGizmos()
+    {
+        int b = 0;
+        if (VerticeList.Count > 0)
+            for (int a = 0; a < VerticeListToShow.Count; a ++)
+            {
+                Gizmos.color = Colors[b++ % Colors.Count];
+                Gizmos.DrawSphere(VerticeListToShow[a], sphereSize);
+            }
+    }
+
+    void Start()
+    {
+        VerticeList = new List<Vector3>(GetComponent<MeshCollider>().sharedMesh.vertices);
+        foreach (Vector3 point in VerticeList)
+        {
+            VerticeListToShow.Add(transform.TransformPoint(point));
+        }
+    }
+
+}
---
+++ Assets/Scripts/ShowCorners.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCorners : MonoBehaviour
+{
+
+    List<Vector3> VerticeList = new List<Vector3>();
+    List<Vector3> VerticeListToShow = new List<Vector3>();
+    public int sphereSize = 1;
+
+    List<Color> CornerColors = new List<Color>() { Color.red, Color.blue, Color.yellow, Color.green };
+    void OnDrawGizmos()
+    {
+        int b = 0;
+        if (VerticeList.Count > 0)
+            for (int a = 0; a < VerticeListToShow.Count; a++)
+            {
+                Gizmos.color = CornerColors[b++];
+                Gizmos.DrawSphere(VerticeListToShow[a], sphereSize);
+            }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VerticeList = new List<Vector3>(GetComponent<MeshCollider>().sharedMesh.vertices);
+
+        SetCornerPoints();  
+    }
+
+    void Update()
+    {
+        SetCornerPoints();
+    }
+
+    public void SetCornerPoints()
+    {
+        VerticeListToShow.Clear();
+
+        VerticeListToShow.Add(transform.TransformPoint(VerticeList[0]));
+        VerticeListToShow.Add(transform.TransformPoint(VerticeList[10]));
+        VerticeListToShow.Add(transform.TransformPoint(VerticeList[110]));
+        VerticeListToShow.Add(transform.TransformPoint(VerticeList[120]));
+    }
+
+}
