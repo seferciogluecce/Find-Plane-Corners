a185de0470a947fe4dd4b00d62f1dfbe11f256f6
Ece Sefercioğlu <seferciogluecce@users.noreply.github.com>
2019-08-04 20:41:20 +0300

    Comments are added to scripts

--- Assets/Scripts/ShowAllVertices.cs
+++ Assets/Scripts/ShowAllVertices.cs
@@ -8,24 +8,24 @@ public class ShowAllVertices : MonoBehaviour
     List<Vector3> VerticeListToShow = new List<Vector3>();
     public float sphereSize = 0.5f;
 
-    List<Color> Colors = new List<Color>() { Color.red, Color.blue, Color.yellow, Color.green, Color.cyan, Color.magenta,Color.gray,Color.black,Color.white,Color.red,Color.blue };
+    List<Color> Colors = new List<Color>() { Color.red, Color.blue, Color.yellow, Color.green, Color.cyan, Color.magenta,Color.gray,Color.black,Color.white,Color.red,Color.blue }; //each color for each column
     void OnDrawGizmos()
     {
-        int b = 0;
+        int b = 0; //b is used to divide points into columns 
         if (VerticeList.Count > 0)
             for (int a = 0; a < VerticeListToShow.Count; a ++)
             {
-                Gizmos.color = Colors[b++ % Colors.Count];
+                Gizmos.color = Colors[b++ % Colors.Count]; 
                 Gizmos.DrawSphere(VerticeListToShow[a], sphereSize);
             }
     }
 
     void Start()
     {
-        VerticeList = new List<Vector3>(GetComponent<MeshCollider>().sharedMesh.vertices);
-        foreach (Vector3 point in VerticeList)
+        VerticeList = new List<Vector3>(GetComponent<MeshCollider>().sharedMesh.vertices); //get vertice points from the mesh of the object
+        foreach (Vector3 point in VerticeList) //all the points are added to be shown on the editor
         {
-            VerticeListToShow.Add(transform.TransformPoint(point));
+            VerticeListToShow.Add(transform.TransformPoint(point)); 
         }
     }
 
--- Assets/Scripts/ShowCorners.cs
+++ Assets/Scripts/ShowCorners.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 public class ShowCorners : MonoBehaviour
 {
 
-    List<Vector3> VerticeList = new List<Vector3>();
+    List<Vector3> VerticeList = new List<Vector3>(); //List of global vertices on the plane
     List<Vector3> VerticeListToShow = new List<Vector3>();
-    public int sphereSize = 1;
+    public int sphereSize = 1; 
 
-    List<Color> CornerColors = new List<Color>() { Color.red, Color.blue, Color.yellow, Color.green };
+    List<Color> CornerColors = new List<Color>() { Color.red, Color.blue, Color.yellow, Color.green }; //Different colors for different corners
     void OnDrawGizmos()
     {
         int b = 0;
@@ -16,7 +16,7 @@ public class ShowCorners : MonoBehaviour
             for (int a = 0; a < VerticeListToShow.Count; a++)
             {
                 Gizmos.color = CornerColors[b++];
-                Gizmos.DrawSphere(VerticeListToShow[a], sphereSize);
+                Gizmos.DrawSphere(VerticeListToShow[a], sphereSize); //show coordinate as a sphere on editor
             }
     }
 
@@ -24,7 +24,7 @@ public class ShowCorners : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        VerticeList = new List<Vector3>(GetComponent<MeshCollider>().sharedMesh.vertices);
+        VerticeList = new List<Vector3>(GetComponent<MeshCollider>().sharedMesh.vertices); //get vertice points from the mesh of the object
 
         SetCornerPoints();  
     }
@@ -36,9 +36,9 @@ public class ShowCorners : MonoBehaviour
 
     public void SetCornerPoints()
     {
-        VerticeListToShow.Clear();
+        VerticeListToShow.Clear(); //incase of transform changes corner points are reset
 
-        VerticeListToShow.Add(transform.TransformPoint(VerticeList[0]));
+        VerticeListToShow.Add(transform.TransformPoint(VerticeList[0])); //corner points are added to show  on the editor
         VerticeListToShow.Add(transform.TransformPoint(VerticeList[10]));
         VerticeListToShow.Add(transform.TransformPoint(VerticeList[110]));
         VerticeListToShow.Add(transform.TransformPoint(VerticeList[120]));
